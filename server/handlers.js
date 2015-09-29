dcdf11bc1eb7d1511c204e4de7559aa6f7c92837
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-29 10:06:01 +0700

    Add size to response json

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var async = require('async');
 
 var IMAGE_DIR = path.join(__dirname, '..', 'images');
 
@@ -9,17 +10,47 @@ handlers.getImage = function (req, res) {
   var imageName = req.params.image;
   var filePath = path.join(IMAGE_DIR, imageName);
 
-  fs.readFile(filePath, function (err, data) {
+  async.parallel({
+
+    file: function readFile(callback) {
+      fs.readFile(filePath, function (err, data) {
+        if (err) {
+          return callback(err);
+        }
+
+        var base64 = (new Buffer(data, 'binary')).toString('base64');
+
+        return callback(null, base64);
+      });
+    },
+
+    size: function getFileSize(callback) {
+      fs.stat(filePath, function (err, stats) {
+        if (err) {
+          return callback(err);
+        }
+
+        return callback(null, bytesToSize(stats.size));
+      });
+
+      // Convert bytes to human readable size.
+      function bytesToSize(bytes) {
+        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        if (bytes == 0) return '0 Byte';
+        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+        return Math.round(bytes / Math.pow(1024, i), 2) + sizes[i];
+      };
+    }
+
+  }, function endParallel(err, result) {
     if (err) {
       return res.json({});
     }
 
-    var base64 = (new Buffer(data, 'binary')).toString('base64');
-
-    res.send({
-      isi_berkas: base64,
+    return res.json({
+      isi_berkas: result.file,
       lokasi_berkas: filePath,
-      ukuran_berkas: ''
+      ukuran_berkas: result.size
     });
   });
 };
