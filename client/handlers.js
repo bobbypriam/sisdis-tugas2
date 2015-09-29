bd4bebff01decc68cd0bed79cd7067fc790b38f4
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-29 11:18:01 +0700

    Implemented client

--- client/handlers.js
+++ client/handlers.js
@@ -1,19 +1,39 @@
+var request = require('request');
+
 var handlers = {};
+
+var API_URL = 'http://localhost:3001/tugas2/server';
 
 var templateHeader = '<!DOCTYPE html><html><head><title>Tugas 2 Sisdis</title></head><body>';
 var templateFooter = '</body></html>';
 
 handlers.getImage = function (req, res) {
-  var html = [
-    templateHeader,
-    '<img id="image" />',
-    '<p id="location"></p>',
-    '<p id="size"></p>',
-    '<p id="not-found"></p>',
-    templateFooter
-  ].join('');
+  var imageName = req.params.image;
+  var type = imageName.split('.')[1];
+  var url = API_URL + '/' + imageName;
 
-  res.send(html);
+  request(url, function (err, response, body) {
+    if (err) {
+      return res.send('error');
+    }
+
+    var data = JSON.parse(body);
+
+    var html = [];
+    html.push(templateHeader);
+
+    if (data.isi_berkas) {
+      html.push('<img id="image" src="data:image/' + type + ';base64,' + data.isi_berkas + '" />');
+      html.push('<p id="location">' + data.lokasi_berkas + '</p>');
+      html.push('<p id="size">' + data.ukuran_berkas + '</p>');
+    } else {
+      html.push('<p id="not-found">The image you requested is not found.</p>');
+    }
+
+    html.push(templateFooter);
+
+    res.send(html.join(''));
+  });
 };
 
 module.exports = handlers;
